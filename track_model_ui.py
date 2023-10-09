ac19f2ff986c1403ba9357547c822b4b24c07228
ralphg1002 <ralphg1002@hotmail.com>
2023-10-09 15:47:24 -0400

    Add all selectable options for track/block info

--- track_model_ui.py
+++ track_model_ui.py
@@ -16,6 +16,9 @@ class SelectionWindow():
         self.add_vline(mainWindow)
         self.add_hline(mainWindow)
         self.add_title(mainWindow)
+        self.add_input_section(mainWindow)
+        self.add_block_info_label(mainWindow)
+        self.add_change_failures_button(mainWindow)
         
         mainWindow.show()
         sys.exit(app.exec_())
@@ -42,10 +45,58 @@ class SelectionWindow():
         title_position = int((window_width - label_width) / 2)
         
         title_label = QLabel("Track Model", parent_window)
-        title_label.setGeometry(title_position, 10, label_width, 30)  # Position and size of the title label
-        title_label.setAlignment(Qt.AlignCenter)  # Center-align the text
-        title_font = QFont("Arial", 20, QFont.Bold)  # Customize the font
+        title_label.setGeometry(title_position, 10, label_width, 30)
+        title_label.setAlignment(Qt.AlignCenter)
+        title_font = QFont("Arial", 20, QFont.Bold)
         title_label.setFont(title_font)
+        
+    def add_input_section(self, parent_window):
+        label = QLabel("Enter Block #:", parent_window)
+        label.setGeometry(970, 70, 150, 30)
+        label.setStyleSheet("font-weight: bold; font-size: 18px")
+        
+        entry_field = QLineEdit(parent_window)
+        entry_field.setGeometry(970, 110, 100, 30)
+        entry_field.setPlaceholderText("Enter block number")
+        
+        button = QPushButton("Go", parent_window)
+        button.setGeometry(1080, 110, 60, 30)
+        button.setStyleSheet("background-color: blue; color: white")
+        
+    def add_block_info_label(self, parent_window):
+        label = QLabel("Block Information:", parent_window)
+        label.setGeometry(970, 160, 200, 30)
+        label.setStyleSheet("font-weight: bold; font-size: 18px")
+        
+        block_info = [
+            "Block Length", "Speed Limit", "Elevation", "Cumulative Elevation",
+            "Block Grade", "Allowed Directions of Travel", "Track Heater",
+            "Failures", "Beacon"
+        ]
+        y_offset = 190
+        for info in block_info:
+            checkbox = QCheckBox(info, parent_window)
+            checkbox.setGeometry(980, y_offset, 200, 30)
+            y_offset += 30
+        
+        track_info = [
+            "Show Occupied Blocks", "Show Switches", 
+            "Show Light Signals", "Show Railway Crossings"
+        ]
+        y_offset += 20
+        for info in track_info:
+            checkbox = QCheckBox(info, parent_window)
+            checkbox.setGeometry(970, y_offset, 200, 30)
+            y_offset += 30
+    
+    def add_change_failures_button(self, parent_window):
+        button = QPushButton("Change Failures ->", parent_window)
+        button.setStyleSheet("background-color: red; color: white")
+        button_width = 200
+        button_height = 30
+        button_x = int(950 + (parent_window.width() - 950 - button_width) / 2)
+        button_y = parent_window.height() - 50
+        button.setGeometry(button_x, button_y, button_width, button_height)
         
         
 if __name__ == '__main__':
